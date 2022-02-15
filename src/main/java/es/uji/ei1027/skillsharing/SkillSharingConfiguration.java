1b54c8738a2102c1efef01a8acc963431b81290b
belen <belen.marco13@gmail.com>
2022-02-15 12:24:36 +0100

    Initial commit

---
+++ src/main/java/es/uji/ei1027/skillsharing/SkillSharingConfiguration.java
@@ -0,0 +1,17 @@
+package es.uji.ei1027.skillsharing;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SkillSharingConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
---
+++ src/main/java/es/uji/ei1027/skillsharing/SkillsharingApplication.java
@@ -0,0 +1,21 @@
+package es.uji.ei1027.skillsharing;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SkillsharingApplication {
+
+	private static final Logger log = Logger.getLogger(SkillsharingApplication.class.getName());
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(SkillsharingApplication.class).run(args);
+	}
+
+	// Funció principal
+	public void run(String... strings) throws Exception {
+		log.info("Aquí va nuestro código");
+	}
+
+}
